45dfd271d2b755cbc406f2ac777270836b7b32a2
Kenneth Schnall <kas6570@psu.edu>
2017-05-11 05:45:30 -0400

    Finish chapter 4 problem 7

---
+++ 04-linked-lists/07-null-or-cycle.md
@@ -0,0 +1,3 @@
+# Null or Cycle
+
+## You are given a linked list with at least one node that is either null-termintated (acyclic) or ends in a cycle (cyclic). Write a function that takes a pointer to the head of a list and determines whether the list is cyclic or acyclic. Your function should return false if the list is acyclic and true if it is cyclic. You may not modify the list in any way.
---
+++ 04-linked-lists/07-null-or-cycle.py
@@ -0,0 +1,23 @@
+def is_cyclic(head):
+  curr = head
+  curr_lagging = head
+
+  if not curr_lagging:
+    return False
+  else:
+    curr = head.next
+
+    while True:
+      if curr is None:
+        return False
+      # If the fast pointer moves onto or over the slow pointer
+      if curr == curr_lagging:
+        return True
+        
+      curr_lagging = curr_lagging.next
+      curr = curr.next
+
+      if curr is not None:
+        curr = curr.next
+      else:
+        return False
