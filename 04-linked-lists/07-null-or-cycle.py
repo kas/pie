cdbba9cd752ede81fb4e160a65bfb6d6a2baf076
Kenneth Schnall <kas6570@psu.edu>
2017-05-11 19:38:29 -0400

    Fix chapter 4 problem 7 for when fast pointer jumps over slow pointer

--- 04-linked-lists/07-null-or-cycle.py
+++ 04-linked-lists/07-null-or-cycle.py
@@ -1,23 +1,28 @@
 def is_cyclic(head):
-  curr = head
-  curr_lagging = head
+    curr = head
+    curr_lagging = head
+    started = False
 
-  if not curr_lagging:
-    return False
-  else:
-    curr = head.next
-
-    while True:
-      if curr is None:
+    if not curr:
         return False
-      # if the fast pointer moves onto or over the slow pointer
-      if curr == curr_lagging:
-        return True
-        
-      curr_lagging = curr_lagging.next
-      curr = curr.next
-
-      if curr is not None:
+    else:
         curr = curr.next
-      else:
-        return False
+
+        while True:
+            if curr is None:
+                return False
+
+            # if the fast pointer moves onto or over the slow pointer
+            if curr == curr_lagging or ((curr == curr_lagging.next) and started):
+                return True
+
+            curr_lagging = curr_lagging.next
+            curr = curr.next
+
+            if curr is not None:
+                curr = curr.next
+            else:
+                return False
+
+            if not started:
+                started = True
